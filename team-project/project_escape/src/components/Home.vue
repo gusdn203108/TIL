d0c2f737160dd5b434984476404ed50fef662424
Hyunwoo Noh <gusdn203108@naver.com>
2022-08-13 14:45:49 +0900

    add_files

---
+++ team-project/project_escape/src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <Todo></Todo>
+        <stopwatch></stopwatch>
+    </div>
+</template>
+<script>
+import Todo from '../components/page3/todo2'
+import stopwatch from '../components/page3/stopwatch.vue'
+
+
+export default {
+    components: {
+    'Todo' : Todo,
+    'stopwatch' : stopwatch
+    },
+};
+
+</script>
+
